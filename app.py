e680a19e07c6eca5930ee04b72146c1666d9bc46
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-25 00:26:14 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify, render_template
+import tensorflow as tf 
+from keras.models import load_model
+from PIL import Image
+from io import BytesIO
+from utils import cleanImage
+
+# loading the models
+mask_detection_model = load_model("./ML/mask_detection.h5") # mask => 0, no_mask => 1
+blur_detection_model = load_model("./ML/blur_sharp_classifier.h5") # blur => 0, sharp => 1
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# mask detectionn
+@app.route('/mask', methods=["GET"])
+def maskForm():
+    return render_template('mask.html')
+
+@app.route('/mask', methods=["POST"])
+def maskDetection():
+    if 'image' not in request.files:
+        return render_template('output.html', output="No file part")
+
+    file = request.files['image']
+
+    if file:
+        img = Image.open(BytesIO(file.read()))
+        img_array = cleanImage(img)
+        output = mask_detection_model.predict(img_array)[0][0]
+
+        if output >= 0.5:
+            output = "No Mask"
+        else:
+            output = "Mask"
+
+        return render_template('output.html', output=output)
+    else:
+        return render_template('output.html', output="No file found")
+
+
+# blur/sharp detection
+@app.route("/blur")
+def blurForm():
+    return render_template('blur.html')
+
+@app.route("/blur", methods=["POST"])
+def blurDetection():
+    if 'image' not in request.files:
+        return render_template('output.html', output="No file part")
+
+    file = request.files['image']
+
+    if file:
+        img = Image.open(BytesIO(file.read()))
+        img_array = cleanImage(img, 224, 224)
+        output = blur_detection_model.predict(img_array)[0][0]
+
+        if output >= 0.5:
+            output = "Sharp"
+        else:
+            output = "Blur"
+
+        return render_template('output.html', output=output)
+    else:
+        return render_template('output.html', output="No file found")
+
+if __name__ == '__main__':
+    app.run(debug=True)
