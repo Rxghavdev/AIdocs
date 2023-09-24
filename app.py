0b459e462026dbf8c94f9c6773453de36d8bd504
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-25 03:19:25 +0530

    add beard detector

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from utils import cleanImage
 # loading the models
 mask_detection_model = load_model("./ML/mask_detection.h5") # mask => 0, no_mask => 1
 blur_detection_model = load_model("./ML/blur_sharp_classifier.h5") # blur => 0, sharp => 1
+beard_detection_model = load_model("./ML/beard_detector.h5") # beard => 0, no_beard => 1 
 
 app = Flask(__name__, template_folder='templates')
 
@@ -67,6 +68,34 @@ def blurDetection():
         return render_template('output.html', output=output)
     else:
         return render_template('output.html', output="No file found")
+
+
+# beard detection
+@app.route("/beard")
+def beardForm():
+    return render_template('beard.html')
+
+@app.route("/beard", methods=["POST"])
+def beardDetection():
+    if 'image' not in request.files:
+        return render_template('output.html', output="No file part")
+
+    file = request.files['image']
+
+    if file:
+        img = Image.open(BytesIO(file.read()))
+        img_array = cleanImage(img, 224, 224)
+        output = beard_detection_model.predict(img_array)[0][0]
+
+        if output >= 0.5:
+            output = "No Beard"
+        else:
+            output = "Beard"
+
+        return render_template('output.html', output=output)
+    else:
+        return render_template('output.html', output="No file found")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
